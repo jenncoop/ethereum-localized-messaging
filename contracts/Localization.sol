9f7e2df9aaeed92a3a378d36c04eae5fcf4fa784
Jenn Cooper <cooper.jennl@gmail.com>
2018-10-05 12:58:26 -0700

    Rename LocalizationPreferences get/1 to textFor/1, natspecs

--- contracts/Localization.sol
+++ contracts/Localization.sol
@@ -1,16 +1,23 @@
 pragma solidity ^0.4.24;
 
+/// @title Holds a simple mapping of codes to their text representations
 contract Localization {
-  mapping(bytes32 => string) internal dictionary_;
+    mapping(bytes32 => string) internal dictionary_;
 
-  constructor() public {}
+    constructor() public {}
 
-   // Currently overwrites anything
-  function set(bytes32 _code, string _message) public {
-    dictionary_[_code] = _message;
-  }
+    /// @notice Sets a mapping of a code to its text representation
+    /// @dev Currently overwrites a code to text representation mapping if it already exists
+    /// @param _code The code with which to associate the text
+    /// @param _message The text representation
+    function set(bytes32 _code, string _message) public {
+        dictionary_[_code] = _message;
+    }
 
-  function textFor(bytes32 _code) external view returns (string _message) {
-    return dictionary_[_code];
-  }
+    /// @notice Fetches the localized text representation.
+    /// @param _code The code to lookup
+    /// @return The text representation for given code, or an empty string
+    function textFor(bytes32 _code) external view returns (string _message) {
+        return dictionary_[_code];
+    }
 }
--- contracts/LocalizationPreferences.sol
+++ contracts/LocalizationPreferences.sol
@@ -2,39 +2,46 @@ pragma solidity ^0.4.24;
 
 import "./Localization.sol";
 
+/// @title A proxy contract that allows users to set their preferred Localization
 contract LocalizationPreferences {
-  mapping(address => Localization) private registry_;
-  Localization public defaultLocalization;
-  bytes32 private empty_ = keccak256(abi.encodePacked(""));
+    mapping(address => Localization) private registry_;
+    Localization public defaultLocalization;
+    bytes32 private empty_ = keccak256(abi.encodePacked(""));
 
-  constructor(Localization _defaultLocalization) public {
-   defaultLocalization = _defaultLocalization;
-  }
-
-  function set(Localization _localization) external returns (bool) {
-    registry_[tx.origin] = _localization;
-    return true;
-  }
-
-  // Primarily for testing
-  function getFor(bytes32 _code, address _who) public view returns (bool, string) {
-    string memory text = getLocalizationFor(_who).textFor(_code);
-    if (keccak256(abi.encodePacked(text)) != empty_) {
-      return (true, text);
-    } else {
-      return (false, defaultLocalization.textFor(_code));
+    constructor(Localization _defaultLocalization) public {
+        defaultLocalization = _defaultLocalization;
     }
-  }
 
-  function get(bytes32 _code) external view returns (bool, string) {
-    return getFor(_code, tx.origin);
-  }
-
-  function getLocalizationFor(address _who) internal view returns (Localization) {
-    if (Localization(registry_[_who]) == Localization(0)) {
-      return Localization(defaultLocalization);
-    } else {
-      return Localization(registry_[tx.origin]);
+    /// @notice Registers a user’s preferred Localization
+    /// @dev The registering user is considered tx.origin
+    /// @param _localization The localization to set for the registering user
+    function set(Localization _localization) external {
+        registry_[tx.origin] = _localization;
     }
-  }
+
+    /// @dev Primarily for testing
+    function get(bytes32 _code, address _who) public view returns (bool, string) {
+        string memory text = getLocalizationFor(_who).textFor(_code);
+
+        if (keccak256(abi.encodePacked(text)) != empty_) {
+            return (true, text);
+        } else {
+            return (false, defaultLocalization.textFor(_code));
+        }
+    }
+
+    /// @notice Retrieve text for a code found at the user’s preferred Localization contract
+    /// @param _code The code to use to retrieve the text representation
+    /// @return (bool, string) tuple: true if found and false otherwise, the corresponding message or default message or empty string
+    function textFor(bytes32 _code) external view returns (bool, string) {
+        return get(_code, tx.origin);
+    }
+
+    function getLocalizationFor(address _who) internal view returns (Localization) {
+        if (Localization(registry_[_who]) == Localization(0)) {
+            return Localization(defaultLocalization);
+        } else {
+            return Localization(registry_[tx.origin]);
+        }
+    }
 }
